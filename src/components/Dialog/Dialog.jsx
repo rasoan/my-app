9a3c18111d079d7a6135b9f70f1c145f2c1dfb67
Araik Rasoyan <araikrasoian@gmail.com>
2020-09-08 22:13:56 +0300

    fix: code

--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -6,24 +6,33 @@ import DialogItem from "../DialogItem";
 
 const Dialog = () => {
   const dialogsData = [
-    {id: '1', name: 'Miha'},
-    {id: '2', name: 'Kostya'},
-    {id: '3', name: 'Vika'},
-    {id: '4', name: 'Tola'},
-    {id: '5', name: 'Mola'},
-    {id: '6', name: 'Natasha'},
-  ]
+    { id: "1", name: "Miha" },
+    { id: "2", name: "Kostya" },
+    { id: "3", name: "Vika" },
+    { id: "4", name: "Tola" },
+    { id: "5", name: "Mola" },
+    { id: "6", name: "Natasha" },
+  ];
+
+  const messagesData = [
+    { id: "1", message: "Hello my friend" },
+    { id: "2", message: "By my friend" },
+    { id: "3", message: "Hello world" },
+    { id: "4", message: "Good bye" },
+    { id: "5", message: "ou sheet!" },
+  ];
 
   return (
     <div className={style.dialogs}>
       <div className={style.dialogsItems}>
-        <DialogItem name="Miha" id="1" />
-        <DialogItem name="Kolya" id="2" />
-        <DialogItem name="Vica" id="3" />
-        <DialogItem name="Tolya" id="4" />
+        {dialogsData.map((element) => (
+          <DialogItem name={element.name} id={element.id} />
+        ))}
       </div>
       <div className={style.messages}>
-        {dialogsData.map( element => <Message name={element.name} id={element.id}/>)}
+        {messagesData.map((element) => (
+          <Message message={element.message} />
+        ))}
       </div>
     </div>
   );
