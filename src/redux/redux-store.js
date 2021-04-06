9f46b69177bb7be63774a3fb86b0163e283b8bbf
Araik Rasoyan <araikrasoian@gmail.com>
2021-04-06 15:04:09 +0300

    add redux extencial

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {combineReducers, createStore} from "redux";
+import {combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import usersReducer from "./users-reducer";
@@ -17,6 +17,7 @@ let reducers = combineReducers({
   appPage: app,
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 window.store = store;
 export default store;
