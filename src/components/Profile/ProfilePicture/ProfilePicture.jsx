89f8517309a967e96b18647e8f35ead3b7524c27
Araik Rasoyan <araikrasoian@gmail.com>
2021-04-08 22:23:50 +0300

    fix profile img

--- src/components/Profile/ProfilePicture/ProfilePicture.jsx
+++ src/components/Profile/ProfilePicture/ProfilePicture.jsx
@@ -1,39 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import style from "./ProfilePicture.module.scss";
-import {InputFile} from "../../../components/common/FormsControls/FormControls";
-import styleForm from "../../common/FormsControls/FormControls.module.scss";
-import { Field, reduxForm } from "redux-form";
-
-const FileUploadForm = ({handleSubmit, error}) => {
-
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <Field placeholder={"Выберите фотографию"}
-               name={"profilePicture"}
-               component={InputFile}
-               type="file" />
-      </div>
-      <button>Сохранить фотографию профиля</button>
-      {error && <div className={styleForm.inCorrectSubmitDataHint}>
-        <p>Выберите файл</p>
-      </div>}
-    </form>
-  );
-};
-
-const AuthorizationFormRedux = reduxForm({form: 'profilePicture'},)(FileUploadForm);
+const fileInputRef = React.createRef();
 
 const ProfilePicture = ({photos}) => {
-  const onSubmit = (formData) => {
-    console.log(formData);
+  const [fileInput, setFileInput] = useState();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setFileInput(fileInputRef.current.files[0]);
   }
 
   return (
     <div className={style.ProfileInfoContainer}>
       <img className={style.image} src={photos.large} />
-      <AuthorizationFormRedux onSubmit={onSubmit} />
+      <form onSubmit={onSubmit}>
+        <input type="file" placeholder="Загрузить файл" ref={fileInputRef} />
+        <button type="submit">Загрузить файл</button>
+      </form>
+        
     </div>
   );
 };
