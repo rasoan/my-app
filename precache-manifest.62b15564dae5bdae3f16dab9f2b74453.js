1aa55cdc37011a2ed3dab563a8dc0996d8d1ddad
Araik Rasoyan <araikrasoian@gmail.com>
2021-04-09 11:39:58 +0300

    Updates

---
+++ precache-manifest.62b15564dae5bdae3f16dab9f2b74453.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3081525053e7bb23bc582987018ff9f8",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "3177c54ffd91ce99e809",
+    "url": "/my-app/static/css/3.f796bb10.chunk.css"
+  },
+  {
+    "revision": "0bcaa9a088aa0ed86c70",
+    "url": "/my-app/static/css/4.cbbbe1e3.chunk.css"
+  },
+  {
+    "revision": "afc5b04b9b66a74e8b7f",
+    "url": "/my-app/static/css/5.95eaf9b8.chunk.css"
+  },
+  {
+    "revision": "c897e76de8956785facd",
+    "url": "/my-app/static/css/main.663f88bc.chunk.css"
+  },
+  {
+    "revision": "9ba066fca104c4ad110f",
+    "url": "/my-app/static/js/2.51339864.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/my-app/static/js/2.51339864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3177c54ffd91ce99e809",
+    "url": "/my-app/static/js/3.f6609580.chunk.js"
+  },
+  {
+    "revision": "0bcaa9a088aa0ed86c70",
+    "url": "/my-app/static/js/4.6b0e7d35.chunk.js"
+  },
+  {
+    "revision": "afc5b04b9b66a74e8b7f",
+    "url": "/my-app/static/js/5.fb60fd6f.chunk.js"
+  },
+  {
+    "revision": "c897e76de8956785facd",
+    "url": "/my-app/static/js/main.1b489dff.chunk.js"
+  },
+  {
+    "revision": "72c531b9193fe471e3ee",
+    "url": "/my-app/static/js/runtime-main.376813f6.js"
+  },
+  {
+    "revision": "d48a8973a98231204ed9be58c6095785",
+    "url": "/my-app/static/media/avatar.d48a8973.png"
+  },
+  {
+    "revision": "a4e6b624090fd5fe96bec8aa96f4512f",
+    "url": "/my-app/static/media/logOut.a4e6b624.png"
+  },
+  {
+    "revision": "66b9eed0d587f983d2dab0e994180317",
+    "url": "/my-app/static/media/signIn.66b9eed0.png"
+  },
+  {
+    "revision": "e6f429188caccbdd634b731de5ddde17",
+    "url": "/my-app/static/media/signUp.e6f42918.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.03a90d50a4c138bf00d05e85af53ab48.js"
+  "/my-app/precache-manifest.62b15564dae5bdae3f16dab9f2b74453.js"
 );
 
 self.addEventListener('message', (event) => {
