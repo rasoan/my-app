8433832b9f8b036e0d6e28777f13215c93828f1a
Araik Rasoyan <araikrasoian@gmail.com>
2021-04-13 17:35:13 +0300

    fix my validation shema

--- src/components/Authorization/AuthorizationPage/AuthorizationForm.jsx
+++ src/components/Authorization/AuthorizationPage/AuthorizationForm.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import styleForm from "../../common/FormsControls/FormControls.module.scss";
-import { Field, reduxForm } from "redux-form";
+import style from "../../common/FormsControls/FormControls.module.scss";
+import { Field, Form, reduxForm } from "redux-form";
 import {signIn} from "../../../redux/auth-reducer";
 import {connect} from "react-redux";
 import {required, maxLengthCreator} from "../../../utils/validators/validators";
 import {Login, Pass} from "../../common/FormsControls/FormControls";
 import { Redirect } from "react-router";
-
 import { useForm } from "react-hook-form";
-import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import classNames from 'classnames';
+
+
+
 
 const maxLength40 = maxLengthCreator(40);
 
@@ -37,7 +41,7 @@ const AuthorizationForm = ({handleSubmit, error, captchaUrl}) => {
         {captchaUrl &&  <Field placeholder={"Введите текст с картинки"}
                                         name={"captcha"}
                                         component={Login} />}
-        {error && <div className={styleForm.inCorrectSubmitDataHint}>
+        {error && <div >
           <p>{error}</p>
         </div>}
       </form>
@@ -49,23 +53,41 @@ export const AuthorizationFormRedux = reduxForm({form: 'authorization'},)(Author
 
 
 export const AuthorizationFormTest = ({handleRegistration, error, captchaUrl}) => {
-    const { register, handleSubmit } = useForm();
+  const validationSchema = Yup.object().shape({
+    login: Yup.string()
+              .min(5, 'Миниум пять символов!')
+              .max(40, 'Максимум 40 символов символов!'),
+    password: Yup.string()
+                 .min(5, 'Миниум пять символов!')
+                 .max(40, 'Максимум 40 символов символов!'),
+  });
+    const { register, formState, handleSubmit } = useForm({
+      resolver: yupResolver(validationSchema)
+  });
+    const {errors, touchedFields, isValid} = formState;
+    const errorClassPassord = classNames({ [style.inCorrect]: errors.password || touchedFields.password && !isValid.password }, 
+                                         { [style.inCorrect]: touchedFields.password && isValid.password});
+
     return (
-        <Form onSubmit={handleSubmit(handleRegistration)}>
+        <form onSubmit={handleSubmit(handleRegistration)}>
           <div>
-            <Input placeholder="Логин"
+            <input placeholder="Логин"
                    type="text"
+                   className={touchedFields.login && !errors.login && style.correct || errors.login && style.inCorrect || style.default }
                    {...register("login")} />
+                   {errors.login && <p>{errors.login.message}</p>}
           </div>
           <div>
-            <Input placeholder="Пароль"
+            <input placeholder="Пароль"
+                   className={errorClassPassord}
                    type="password"
                    {...register("password")} />
+                   {errors.password && <p>{errors.password.message}</p>}
           </div>
           <div>
             <input type="checkbox" {...register("rememberMe")} /> запомнить меня
           </div>
-          <Button>Залогиниться</Button>
-        </Form>
+          <button>Залогиниться</button>
+        </form>
       );
 }
