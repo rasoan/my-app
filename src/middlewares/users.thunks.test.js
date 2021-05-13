3d94013f146e29648b8feee97c8aa8c663bd636b
Araik Rasoyan <araikrasoian@gmail.com>
2021-05-13 17:41:32 +0300

    fix test users midleware

--- src/middlewares/users.thunks.test.js
+++ src/middlewares/users.thunks.test.js
@@ -1,3 +1,27 @@
-test("Тестируем санку, которая отвечает за подписку на пользователя: ", () => {
+import {follow} from "./users";
+import {usersApi} from "../api/api";
+import {
+  followAC,
+  isFetchingFollowOrUnfollowEnd,
+  isFetchingFollowOrUnfollowStart
+} from "../redux/actions/creators/users-creator";
 
+jest.mock("../api/api");
+const userApiMock = usersApi;
+const result = { data: { resultCode: 0 } };
+
+userApiMock.follow.mockReturnValue(Promise.resolve(result));
+
+
+test("Тестируем санку, которая отвечает за подписку на пользователя: ", async () => {
+  const thunk = follow(1);
+  const dispatchMock = jest.fn();
+
+  await thunk(dispatchMock);
+
+
+  expect(dispatchMock).toBeCalledTimes(3);
+  expect(dispatchMock).toHaveBeenNthCalledWith(1, isFetchingFollowOrUnfollowStart(1));
+  expect(dispatchMock).toHaveBeenNthCalledWith(2, followAC(1));
+  expect(dispatchMock).toHaveBeenNthCalledWith(3, isFetchingFollowOrUnfollowEnd(1));
 });
