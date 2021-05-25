4c04e9a2b88ac54fb9d719541fe138da46ea6aae
Araik Rasoyan <araikrasoian@gmail.com>
2021-05-25 13:43:33 +0300

    fix users index.js

--- src/components/Users/UsersList/index.js
+++ src/components/Users/UsersList/index.js
@@ -1,72 +1,38 @@
 import React, {useEffect} from "react";
-import PropTypes from "prop-types";
 import UsersList from "./UsersList";
 import {connect} from "react-redux";
-import {
-    unfollow, follow, getCountUsers, getUsersCardsSC
-} from '../../../middlewares/users';
-import {getUsers, getPagesSize, getTotalUsersCount, 
-        getIsFetchingFollowOrUnfollowIdList,
-        getIsAuth, getDefaultAvatarSrc, getIsFetchingGetUsersCards, getIsFetchingGetUsersCount} from "../../../selectors/users-selectors";
+import {unfollow, follow, getCountUsers, getUsersCardsSC} from '../../../middlewares/users';
 import {startCommunication} from "../../../middlewares/dialogs";
-
-
-
-const UsersListContainer = (props) => {
-  const {pagesSize, totalUsersCount, users,
-         isFetchingFollowOrUnfollowIdList, follow,
-         unfollow, defaultAvatarSrc, isAuth, startCommunication,
-      isFetchingGetUsersCards, isFetchingGetUsersCount,
-      getUsersCardsSC} = props;
-
-
-
-    return (<UsersList totalUsersCount={totalUsersCount}
-                       pagesSize={pagesSize}
-                       users={users}
-                       isFetchingFollowOrUnfollowIdList={isFetchingFollowOrUnfollowIdList}
-                       getUsersCardsSC={getUsersCardsSC}
-                       isFetchingGetUsersCount={isFetchingGetUsersCount}
-                       isFetchingGetUsersCards={isFetchingGetUsersCards}
-                       follow={follow} 
-                       unfollow={unfollow} 
-                       defaultAvatarSrc={defaultAvatarSrc}
-                       isAuth={isAuth}
-                       startCommunication={startCommunication} />); 
-}
-
-UsersListContainer.propTypes = {
-  currentPage: PropTypes.number,
-  pagesSize: PropTypes.number,
-  totalUsersCount: PropTypes.number,
-  users: PropTypes.array,
-  isFetchingFollowOrUnfollowIdList: PropTypes.array,
-  follow: PropTypes.func,
-  unfollow: PropTypes.func,
-  defaultAvatarSrc: PropTypes.string,
-  isAuth: PropTypes.bool,
-  startCommunication: PropTypes.func,
-}
+import {
+    getUsers,
+    getPagesSize,
+    getTotalUsersCount,
+    getIsFetchingFollowOrUnfollowIdList,
+    getIsAuth,
+    getDefaultAvatarSrc,
+    getIsFetchingGetUsersCards,
+    getIsFetchingGetUsersCount
+} from "../../../selectors/users-selectors";
 
 const mapStateToProps = (state) => {
-  return {
-          users: getUsers(state),
-          pagesSize: getPagesSize(state),
-          totalUsersCount: getTotalUsersCount(state),
-          isFetchingFollowOrUnfollowIdList: getIsFetchingFollowOrUnfollowIdList(state),
-          isFetchingGetUsersCards: getIsFetchingGetUsersCards(state),
-          isFetchingGetUsersCount: getIsFetchingGetUsersCount(state),
-          isAuth: getIsAuth(state),
-          defaultAvatarSrc: getDefaultAvatarSrc(state),
-         }
+    return {
+        users: getUsers(state),
+        pagesSize: getPagesSize(state),
+        totalUsersCount: getTotalUsersCount(state),
+        isFetchingFollowOrUnfollowIdList: getIsFetchingFollowOrUnfollowIdList(state),
+        isFetchingGetUsersCards: getIsFetchingGetUsersCards(state),
+        isFetchingGetUsersCount: getIsFetchingGetUsersCount(state),
+        isAuth: getIsAuth(state),
+        defaultAvatarSrc: getDefaultAvatarSrc(state),
+    }
 }
 
 const actionCreators = {
-  follow,
-  unfollow,
-  startCommunication,
+    follow,
+    unfollow,
+    startCommunication,
     getCountUsers,
     getUsersCardsSC
 }
 
-export default connect(mapStateToProps, actionCreators)(UsersListContainer);
+export default connect(mapStateToProps, actionCreators)(UsersList);
--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -1,27 +1,2 @@
-import React from "react";
-import PropTypes from "prop-types";
 import UsersList from "./UsersList";
-import { connect } from "react-redux";
-
-const Users = (props) => {
-  const {isAuth} = props;
-
-    if (!isAuth) {
-      console.log("Пользователь анонимный, надо запретить добавлять в друзья и так далее")
-    }
-    return (
-      <>
-        <UsersList />
-      </>
-    );
-}
-
-Users.propTypes = {
-  isAuth: PropTypes.bool,
-}
-
-let mapStateToProps = (state) => ({
-  isAuth: state.auth.isAuth,
-});
-
-export default connect(mapStateToProps, null)(Users);
+export default UsersList;
