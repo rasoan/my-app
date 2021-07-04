5057e10c46be48bae4c984838ee3de4ba4b7ab0f
Araik Rasoyan <araikrasoian@gmail.com>
2021-07-04 14:31:35 +0300

    fix my uploader file

--- src/components/UploaderPhoto/UploaderPhoto.jsx
+++ src/components/UploaderPhoto/UploaderPhoto.jsx
@@ -1,16 +1,37 @@
 import React, {useState} from "react";
+import style from "./UploaderPhoto.module.scss"
 import Dropzone from "react-dropzone";
 import ReactAvatarEditor from "react-avatar-editor";
 import ButtonRange from "../ButtonRange";
 import Button from "../Button";
-import {Container, Grid, GridList} from "@material-ui/core";
+import {Container, Grid, GridList, Paper} from "@material-ui/core";
 import Box from "@material-ui/core/Box";
+import {makeStyles} from '@material-ui/core';
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+    root: {
+        padding: 30,
+    },
+    uploadFileBox: {
+        width: "100%",
+        height: "100%",
+        diaplay: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    uploadFileBoxOnDrop: {
+        backgroundColor: "#2ce4058f",
+    }
+});
 
 const UploaderPhoto = (props) => {
     const {updatePhotoFunction} = props;
+    let classes = useStyles({border: "1px solid black"});
     const [image, setImage] = useState(null);
     const [scale, setScale] = useState(1);
     const [editor, setEditor] = useState(null);
+    const [dragOver, setDragOver] = useState(false);
     const uploadFileRef = React.useRef();
 
     const handleDown = () => {
@@ -43,38 +64,60 @@ const UploaderPhoto = (props) => {
     };
 
     const handleDrop = (acceptedFiles) => {
-        console.log("hello")
+        setDragOver(false);
         setImage(acceptedFiles[0]);
     };
 
+    const dragOverHandler = () => {
+        setDragOver(true);
+        console.log("Изображение наведено");
+    }
+
+    const dragLeaveHandler = () => {
+        setDragOver(false);
+        console.log("Изображение покинуло поле");
+    }
+// `${classes.root} ${dragOver ? classes.dragOver : !image ? classes.dragLeave: classes.dragDefault}`
     return <div>
-        <Dropzone onDrop={handleDrop}>
-            {({ getRootProps, getInputProps }) => (
-                <div {...getRootProps({ className: "dropzone" })}>
+        <Dropzone onDrop={handleDrop}
+                  onDragEnter={dragOverHandler}
+                  onDragLeave={dragLeaveHandler}>
+            {({getRootProps, getInputProps}) => (
+                <div style={{width: 320, height: 320, display: "flex", justifyContent: "center", alignItems: "center"}}
+                     {...getRootProps({className: "dropzone"})}
+                     onClick={() => uploadFileRef.current.click()}
+                     className={`${style.dropzone} ${dragOver ? style.dropzoneDragOver : !image ? style.dropzoneDragLeave : style.transparentBorder}`}
+                >
                     <input {...getInputProps()}
-                           ref={uploadFileRef} />
-                    <ReactAvatarEditor
+                           ref={uploadFileRef}/>
+                    {image && <ReactAvatarEditor
                         ref={setEditorRef}
                         scale={parseFloat(scale)}
-                        width={270}
-                        height={300}
+                        width={250}
+                        height={250}
                         rotate={0}
-                        border={25}
+                        border={20}
                         image={image}
                         className="editor-canvas"
-                    />
+                    />}
+                    {!image && <Box className={`${classes.uploadFileBox} ${dragOver && classes.uploadFileBoxOnDrop}`}
+                                    display={"flex"}>
+                        <Typography variant="h8" component="span">Загрузить фотографию</Typography>
+                    </Box>}
+                    <div
+                        className={`${style.dropzoneBottomRL} ${dragOver ? style.dropzoneBottomRLDragOver : !image ? style.dropzoneBottomRLDragLeave : style.transparentBorder}`}/>
                 </div>
             )}
         </Dropzone>
 
         <Box display="flex" justifyContent="center" alignItems="center">
-        <ButtonRange onChange={handleScale}
-                     min={1}
-                     max={5}
-                     step={0.01}
-                     width={250}
-                     text={"Зум"}
-        />
+            <ButtonRange onChange={handleScale}
+                         min={1}
+                         max={5}
+                         step={0.01}
+                         width={250}
+                         text={"Зум"}
+            />
         </Box>
         <Grid container justify={"center"} spacing={3}>
             {!image && <Grid item>
