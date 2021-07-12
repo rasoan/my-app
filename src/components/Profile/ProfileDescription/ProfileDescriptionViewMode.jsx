d15f740a8c805fd89f0af1459e03ff60ab85e8d0
Araik Rasoyan <araikrasoian@gmail.com>
2021-07-12 17:26:15 +0300

    fix my description profile

--- src/components/Profile/ProfileDescription/ProfileDescriptionViewMode.jsx
+++ src/components/Profile/ProfileDescription/ProfileDescriptionViewMode.jsx
@@ -6,7 +6,22 @@ import {Box, makeStyles} from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 
-const useStyles = makeStyles({
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import InsertLinkIcon from '@material-ui/icons/InsertLink';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import StarBorder from '@material-ui/icons/StarBorder';
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles(theme => ({
     root: {
         backgroundColor: 'white',
     },
@@ -21,36 +36,75 @@ const useStyles = makeStyles({
     },
     fullName: {
         marginBottom: 4,
+    },
+    nested: {
+        paddingLeft: theme.spacing(4),
+    },
+    listItemContainer: {
+        width: "100%"
+    },
+    collapsibleButtonLinks: {
+        display: "flex",
+        justifyContent: "space-between",
+        '&:hover': {
+            backgroundColor: theme.backgroundColors.mainBackgroundColor,
+        }
     }
-});
+}));
 
 const ProfileDescriptionViewMode = (props) => {
     const {profile} = props;
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
     return (<>
-        <Box p={2} m={2} width={540} height={446} className={classes.root}>
-            <Box mb={2}>
-                <Typography className={classes.fullName} variant={"h5"} component={"p"}>{profile.fullName}</Typography>
-                <Status/>
+            <Box p={2} m={2} width={540} height={"min-content"} className={classes.root}>
+                <Box mb={2}>
+                    <Typography className={classes.fullName} variant={"h5"}
+                                component={"p"}>{profile.fullName}</Typography>
+                    <Status/>
+                </Box>
+                <List disablePadding>
+                    {profile.lookingForAJob && <ListItem>
+                        <Typography><b>Работа: </b>{profile.lookingForAJobDescription}</Typography>
+                    </ListItem>}
+                    <ListItem>
+                        <Typography><b>О себе:</b> {profile.aboutMe}</Typography>
+                    </ListItem>
+                        <Divider/>
+                    <ListItem style={{display: "block"}}>
+                        <Button onClick={handleClick}
+                                fullWidth
+                                className={classes.collapsibleButtonLinks}>
+                            <Box display={"flex"}>
+                                <InsertLinkIcon style={{marginRight: 10}}/>
+                                <Typography>Мои социальные сети:</Typography>
+                            </Box>
+                            {open ? <ExpandLess/> : <ExpandMore/>}
+                        </Button>
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                            <List style={{paddingBottom: 0}}>
+                                {profile?.contacts && Object.keys(profile.contacts).map((key, i) => {
+                                    return (<>
+                                        {profile.contacts[key] && <ListItem key={i + key}>
+                                            <ListItemText>
+                                                <Typography><b>{key}: </b>{profile.contacts[key]}</Typography>
+                                            </ListItemText>
+                                        </ListItem>}
+                                    </>)
+                                })}
+                            </List>
+                            <Divider/>
+                        </Collapse>
+                    </ListItem>
+                </List>
             </Box>
-            <p><b>Looking for a job:</b> {profile.lookingForAJob ? "Yes" : "No"}</p>
-            {profile.lookingForAJob &&
-            <div>
-                <p>{profile.lookingForAJobDescription}</p>
-            </div>}
-            <div>
-                <p><b>About me:</b> {profile.aboutMe}</p>
-            </div>
-            <div>
-                <b>Contacts:</b>
-                <ul>{profile?.contacts && Object.keys(profile.contacts).map((key, i) => {
-                    return <li key={i + key}>
-                        <b>{key}:</b> {profile.contacts[key]}
-                    </li>
-                })}</ul>
-            </div>
-        </Box>
-    </>);
+        </>
+    );
 }
 
 ProfileDescriptionViewMode.propTypes = {
